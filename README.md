1995569d01e93b6f9e4ddf5413ce718fee2a677b
Jari Matilainen <vague@cpan.org>
2025-11-26 11:16:59 +0100

    Initial commit - Minilla scaffold

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Mojo::URL::Role::Auth - It's new $module
+
+# SYNOPSIS
+
+    use Mojo::URL::Role::Auth;
+
+# DESCRIPTION
+
+Mojo::URL::Role::Auth is ...
+
+# LICENSE
+
+Copyright (C) vague666.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+vague666 <>
---
+++ lib/Mojo/URL/Role/Auth.pm
@@ -0,0 +1,39 @@
+package Mojo::URL::Role::Auth;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojo::URL::Role::Auth - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mojo::URL::Role::Auth;
+
+=head1 DESCRIPTION
+
+Mojo::URL::Role::Auth is ...
+
+=head1 LICENSE
+
+Copyright (C) vague666.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+vague666 E<lt>E<gt>
+
+=cut
+
