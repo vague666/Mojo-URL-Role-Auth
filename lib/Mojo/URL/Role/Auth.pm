cc1b5a20cfa0e9710b20dbdce66c3a2083a2305e
Jari Matilainen <vague@cpan.org>
2025-11-26 11:45:57 +0100

    Updated documentation

--- lib/Mojo/URL/Role/Auth.pm
+++ lib/Mojo/URL/Role/Auth.pm
@@ -1,11 +1,29 @@
 package Mojo::URL::Role::Auth;
-use 5.008001;
+
 use strict;
 use warnings;
+use Mojo::Base -role;
+has [qw(username password)];
 
-our $VERSION = "0.01";
+our $VERSION = '0.1.0';
 
+sub auth {
+  my ($self, $user, $pass)  = @_;
 
+  if(!defined $user && !defined $pass) {
+    return undef unless defined $self->username;
+    return $self->username unless defined $self->password;
+    return sprintf("%s:%s", $self->username, $self->password);
+  }
+  elsif(!defined $pass) {
+    ($user, $pass) = split ':', $user, 2;
+  }
+
+  $self->username($user);
+  $self->password($pass);
+
+  return $self->userinfo($self->auth);
+}
 
 1;
 __END__
@@ -14,26 +32,35 @@ __END__
 
 =head1 NAME
 
-Mojo::URL::Role::Auth - It's new $module
+Mojo::URL::Role::Auth - Use 2-arg function to add userinfo to url
 
 =head1 SYNOPSIS
 
-    use Mojo::URL::Role::Auth;
+    my $url = Mojo::URL->new('https://example.com')->with_roles('+Auth');
+    $url->auth('u53rn4m3', 'p455w0rd');
+    say $url->to_unsafe_string; # gives https://u53rn4m3:p455w0rd@example.com
 
 =head1 DESCRIPTION
 
-Mojo::URL::Role::Auth is ...
+This role adds a new method that takes two arguments to set userinfo for a url
+
+=head1 METHODS
+
+=head2 auth
+
+    my $url = Mojo::URL->new->with_roles('+Auth');
+    $url->auth('u53rn4m3', 'p455w0rd');
 
 =head1 LICENSE
 
-Copyright (C) vague666.
+Copyright (C) Jari Matilainen.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =head1 AUTHOR
 
-vague666 E<lt>E<gt>
+vague E<lt>vague@cpan.orgE<gt>
 
 =cut
 
